d3f03e5ebfaf73860dfae4296b9306efbd1050f9
ArsyaFows <arsyafauz@gmail.com>
2025-08-29 17:27:53 +0700

    tuker button

--- tes-amazink/src/views/AppLayout.vue
+++ tes-amazink/src/views/AppLayout.vue
@@ -17,7 +17,12 @@
   <button @click="toggleMenu" class="md:hidden p-2 rounded-lg bg-blue-500 hover:bg-blue-600 text-white">
     ☰
   </button>
-
+  <!-- Dark/Light desktop -->
+  <button 
+    @click="toggleDarkMode" 
+    class="px-3 py-2 rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white hidden md:block">
+    {{ darkMode ? '☀️ Light' : '🌙 Dark' }}
+  </button>
   <!-- Logout desktop -->
   <button 
     @click="$emit('logout')" 
@@ -25,12 +30,7 @@
     🔒 Logout
   </button>
 
-  <!-- Dark/Light desktop -->
-  <button 
-    @click="toggleDarkMode" 
-    class="px-3 py-2 rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white hidden md:block">
-    {{ darkMode ? '☀️ Light' : '🌙 Dark' }}
-  </button>
+
 </div>
 
     </div>
